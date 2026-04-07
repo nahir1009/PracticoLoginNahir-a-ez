6363461c36a0fddda22f7cec8e10ca4643fa36cf
nahir1009 <nahir1009@gmail.com>
2026-04-07 00:31:07 -0300

    agrego urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import RegisterView, LoginView, RequestOTPView, VerifyOTPView, ResetPasswordView
-
+#url corregidas
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('otp/request/', RequestOTPView.as_view(), name='otp-request'),
     path('otp/verify/', VerifyOTPView.as_view(), name='otp-verify'),
     path('otp/reset-password/', ResetPasswordView.as_view(), name='otp-reset-password'),
+    
 ]
